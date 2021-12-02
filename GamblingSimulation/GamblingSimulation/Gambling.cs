b970c83f153c3fb056604cc29e49b87ab84ff815
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-12-02 18:27:05 +0530

    uc1 done

---
+++ GamblingSimulation/GamblingSimulation/Gambling.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblingSimulation
+{
+   public class Gambling
+    {
+        public int stake = 100;
+        public int bet = 1;
+    }
+}
