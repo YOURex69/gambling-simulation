b8a6559685dd0ca3d41f8b9cceb76f89e9148853
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-12-02 19:15:49 +0530

    Added Uc2 gambling simulation for checking win or loose

---
+++ GamblingSimulation/GamblingSimulation/Gambling.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblingSimulation
+{
+   public class Gambling
+    {
+        public int stake = 100;
+        public int bet = 1;
+       public const int WIN = 1;
+       public const int LOOSE = 0;
+        public void check()
+        {
+            Random rand = new Random();
+            int check = rand.Next(0, 2);
+            if (check == WIN)
+            {
+                stake = stake + 1;//wins
+            }
+            else if (check == LOOSE)
+            {
+                stake = stake - 1;//looses
+            }
+
+        }
+    }
+}
